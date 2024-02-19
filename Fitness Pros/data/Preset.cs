3ec3be4d93a5ce074a6d747f36b07cd17bcb3340
CallumBelfield <151842508+CallumBelfield@users.noreply.github.com>
2024-02-19 15:37:06 +0000

    Version 0.00.01
    Created the basics for a functioning application. Coded a preset/profiles class and functionality.

---
+++ Fitness Pros/data/Preset.cs
@@ -0,0 +1,9 @@
+namespace MyBlazorApp.Data
+{
+    public class Preset
+    {
+        public int Weight { get; set; }
+        public int CalorieIntake { get; set; }
+        public int Height { get; set; }
+    }
+}
