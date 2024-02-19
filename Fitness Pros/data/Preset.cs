80cba3d8fdc2fc64e690427bbe9f79558f2c9f4c
CallumBelfield <151842508+CallumBelfield@users.noreply.github.com>
2024-02-19 16:27:35 +0000

    Error in VSC generated code.

--- Fitness Pros/data/Preset.cs
+++
@@ -1,9 +0,0 @@
-namespace MyBlazorApp.Data
-{
-    public class Preset
-    {
-        public int Weight { get; set; }
-        public int CalorieIntake { get; set; }
-        public int Height { get; set; }
-    }
-}
